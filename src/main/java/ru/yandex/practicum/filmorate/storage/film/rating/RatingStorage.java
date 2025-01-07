86a10ead983004ab2dd694c5a06c52a9d12c1f8b
Anasasiia Novikova <kimana_meow@bk.ru>
2025-01-07 06:19:26 +0300

    Исправить кодстайл

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/rating/RatingStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/rating/RatingStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.film.rating;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.util.List;
-import java.util.Optional;
 
 
 public interface RatingStorage {
